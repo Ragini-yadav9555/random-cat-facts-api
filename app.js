1ff11dbb204734923132202a65728d45ac15a8bd
Ragini-yadav9555 <raginiy283@gmail.com>
2026-06-17 20:20:03 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async() => {
+    let fact = await getFacts();
+
+    let p = document.querySelector("#facts");
+    p.innerText = fact;
+
+})
+
+
+let url = "https://catfact.ninja/fact";
+
+async function getFacts() {
+    try {
+        let res = await axios.get(url);
+        return res.data.fact;
+    } catch (err) {
+        console.log("Error : ", err);
+        return "No Fact Found!!!";
+    }
+}
